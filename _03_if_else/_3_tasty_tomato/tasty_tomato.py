4ced6a5dd19181e7bea7fa3950ba9338350c4a3f
league <karinabriellen@gmail.com>
2023-11-12 13:00:21 -0800

    tomato

--- _03_if_else/_3_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_3_tasty_tomato/tasty_tomato.py
@@ -10,22 +10,39 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=window_width, height=window_height, bg="#DDDDDD")
 canvas.grid()
 
+if __name__ == '__main__':
+
+
 # 1. Ask the user what color tomato they would like and save their response
 #    You can give them up to three choices
-    messagebox.showinfo(title=''message='You have a tomato...')
-    x = simpledialog.askstring(title='', message='What color do you want your tomato to be?')
-    y = simpledialog.askstring(title='', message='Is there another color you want your tomato to be?')
-    if x == 'yes':
-        a = simpledialog.askstring(title='', message='What other color do you want your tomato to be?')
-    z = simpledialog.askstring(title='', message='Is there one more color you want your tomato to be?')
-    if x == 'yes':
-        b = simpledialog.askstring(title='', message='What is one more color you want your tomato to be?')
-    else : messagebox.showinfo()
+    messagebox.showinfo(title='', message='You have a tomato...')
+    x = simpledialog.askstring(title='', prompt='What color do you want your tomato to be?')
+    a = simpledialog.askstring(title='', prompt='Is there another color you want your tomato to be?')
+    if a == 'yes':
+        y = simpledialog.askstring(title='', prompt='What other color do you want your tomato to be?'
+                                                  
+'(remember to think of all the colors in a tomato, even the colors in the stem!)')
+        b = simpledialog.askstring(title='', prompt='Is there one more color you want your tomato to be?')
+        if b == 'yes':
+            z = simpledialog.askstring(title='', prompt='What is one more color you want your tomato to be?')
+    else : messagebox.showinfo(title='',message="Let's start with our tomato!")
 # 2. Use if-else statements to draw the tomato in the color that they chose
 #    You can modify the code below or draw your own tomato
-canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
-canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
+    if a == 'no':
+        canvas.create_rectangle(275, 100, 300, 350, fill= x, outline="")
+        canvas.create_oval(75, 200, 400, 450, fill= x, outline="")
+        canvas.create_oval(200, 200, 525, 450, fill= x, outline="")
+    if a == 'yes':
+        if b == 'no':
+            canvas.create_rectangle(275, 100, 300, 250, fill=y, outline="")
+            canvas.create_oval(75, 200, 400, 450, fill=x, outline="")
+            canvas.create_oval(200, 200, 525, 450, fill=x, outline="")
+    if a == 'yes':
+        if b == 'yes':
+            canvas.create_rectangle(275, 100, 300, 250, fill=y, outline="")
+            canvas.create_oval(75, 200, 400, 450, fill=x, outline="")
+            canvas.create_oval(200, 200, 525, 450, fill=z, outline="")
 
-canvas.create_rectangle(275, 100, 325, 230, fill="green", outline="")
+
 
 root.mainloop()
--- _03_if_else/_4_circle_calculator/circle_calculator.py
+++ _03_if_else/_4_circle_calculator/circle_calculator.py
@@ -1,7 +1,12 @@
 # Write a Python program that asks the user for the radius of a circle.
 # Next, ask the user if they would like to calculate the area or circumference of a circle.
-# If they choose area, display the area of the circle using the radius.
+# Iif they choose area, display the area of the circle using the radius.
 # Otherwise, display the circumference of the circle using the radius.
+if __name__ == '__main__':
+    k = simpledialog.askstring(title = "", prompt = "What is a random radius of a circle?...")
+   question = simpledialog.askstring(title = "", prompt = "What would you like to calculate more, area, or circumference of a circle?")
+    if question == "area" :
+        math.pi * k * k
 
 #Area = πr^2
 #Circumference = 2πr 
