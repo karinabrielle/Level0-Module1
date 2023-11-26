c61351380ae726e486c489b804e629ef2c75ae7a
league <karinabriellen@gmail.com>
2023-11-26 13:01:57 -0800

    today was fun

--- _03_if_else/_5_shape_selector/shape_selector.py
+++ _03_if_else/_5_shape_selector/shape_selector.py
@@ -9,9 +9,34 @@ if __name__ == '__main__':
     window.withdraw()
     
     # Make a new turtle
-    
+    hi = turtle.Turtle()
     # Ask the user what shape they want to draw and store it in a variable
-    
+    question = simpledialog.askstring(title="", prompt='What type of shape should I draw? Please just say the shape with no other words!')
     # Draw the shape requested by the user using if-elif-else statements
-    
-    # Call the turtle .done() method
+    if question == 'square' :
+        for i in range(4):
+            hi.forward(50)
+            hi.right(90)
+            hi.forward(50)
+    if question == 'rectangle':
+        for i in range(2):
+            hi.forward(150)
+            hi.right(90)
+            hi.forward(30)
+            hi.right(90)
+    if question == 'diamond':
+        for i in range(2):
+            hi.right(45)
+            hi.forward(80)
+            hi.right(135)
+            hi.forward(80)
+    if question == 'triangle':
+            hi.right(55)
+            hi.forward(100)
+            hi.right(125)
+            hi.forward(100)
+            hi.right(125)
+            hi.forward(100)
+    # Call the turtle.done() method
+
+    turtle.done()
